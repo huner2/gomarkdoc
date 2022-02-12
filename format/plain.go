b9c2fbd9fea81ff43d08641ec837b341ecfe6309
Hunter Ross <hross@sensilla.cloud>
2022-02-11 20:14:44 -0600

    Fix navigation

--- format/plain.go
+++ format/plain.go
@@ -37,13 +37,56 @@ func (f *PlainMarkdown) RawHeader(level int, text string) (string, error) {
 // LocalHref always returns the empty string, as header links are not supported
 // in plain markdown.
 func (f *PlainMarkdown) LocalHref(headerText string) (string, error) {
-	return "", nil
+	result := formatcore.PlainText(headerText)
+	result = strings.ToLower(result)
+	result = strings.TrimSpace(result)
+	result = gfmWhitespaceRegex.ReplaceAllString(result, "-")
+	result = gfmRemoveRegex.ReplaceAllString(result, "")
+
+	return fmt.Sprintf("#%s", result), nil
 }
 
 // CodeHref always returns the empty string, as there is no defined file linking
 // format in standard markdown.
 func (f *PlainMarkdown) CodeHref(loc lang.Location) (string, error) {
-	return "", nil
+	// If there's no repo, we can't compute an href
+	if loc.Repo == nil {
+		return "", nil
+	}
+
+	var (
+		relative string
+		err      error
+	)
+	if filepath.IsAbs(loc.Filepath) {
+		relative, err = filepath.Rel(loc.WorkDir, loc.Filepath)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		relative = loc.Filepath
+	}
+
+	full := filepath.Join(loc.Repo.PathFromRoot, relative)
+	p, err := filepath.Rel(string(filepath.Separator), full)
+	if err != nil {
+		return "", err
+	}
+
+	var locStr string
+	if loc.Start.Line == loc.End.Line {
+		locStr = fmt.Sprintf("L%d", loc.Start.Line)
+	} else {
+		locStr = fmt.Sprintf("L%d-L%d", loc.Start.Line, loc.End.Line)
+	}
+
+	return fmt.Sprintf(
+		"%s/blob/%s/%s#%s",
+		loc.Repo.Remote,
+		loc.Repo.DefaultBranch,
+		filepath.ToSlash(p),
+		locStr,
+	), nil
 }
 
 // Link generates a link with the given text and href values.
